4ad36dfe0bc91575f334c2561cf5bf30788cb86f
Ken Holston <git.kholston@gmail.com>
2020-05-14 14:09:23 -0400

    Create initial classes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,15 @@
 require_relative 'node'
 
 class LinkedList
+  attr_accessor :head, :tail
+
+  def initialize(head = nil, tail = nil)
+    @head = head
+    if !@head.nil? && tail.nil?
+      @tail = @head
+    else
+      @tail = tail
+    end
+  end
+
 end
