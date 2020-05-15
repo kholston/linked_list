4d659bea1f9911f0f84335e2b39d58057a0d9ccd
Ken Holston <git.kholston@gmail.com>
2020-05-15 16:39:37 -0400

    Refacter linked_list_test.rb

--- linked_list_test.rb
+++ linked_list_test.rb
@@ -1,43 +1,45 @@
-require_relative 'linked_list'
+require_relative 'lib/linked_list'
 
-list = LinkedList.new
+new_list = LinkedList.new
 
-puts 'List created' unless list.nil?
+puts 'List created' unless new_list.nil?
 
 puts 'Append'
-list.append('first')
-puts 'Added node'
-ç
+new_list.append('first')
+puts 'Added node at end'
 
 puts 'Prepend'
-list.prepend('second')
-puts 'Added node'
-puts "Current amount of nodes is #{list.size}"
+new_list.prepend('second')
+puts 'Added node at start'
+puts "Current amount of nodes is #{new_list.size}"
 
 puts 'Head of list'
-puts list.head
+puts new_list.head.value
 
 puts 'Tail of list'
-puts list.tail
+puts new_list.tail.value
 
 puts 'Add 2 more nodes'
-list.append('third')
-list.append('fourth')
-puts "Current amount of nodes is #{list.size}"
+new_list.append('third')
+new_list.append('fourth')
+puts "Current amount of nodes is #{new_list.size}"
+puts "The full list is #{new_list}"
 
 puts 'Return node at index'
 puts 'The node at index 2 value should be third'
-puts "The node at index 2 is #{list.at(2)}"
+puts "The node at index 2 is #{new_list.at(2).value}"
 
 puts 'Remove the last node'
-list.pop
-puts "Current amount of nodes is #{list.size}"
+new_list.pop
+puts "Current amount of nodes is #{new_list.size}"
+puts "The full list is #{new_list}"
 
 puts 'Checks for first value'
-puts "It is #{list.contains('first')} that the list contains the value first"
-puts "It is #{list.contains('fifth')} that the list contains the value fifth"
+puts "It is #{new_list.contains('first')} that the list contains the value first"
+puts "It is #{new_list.contains('fifth')} that the list contains the value fifth"
 
-puts 'Return node at index'
-puts "The node at index 3 is #{list.find(3)}"
+puts 'Return index of node'
+puts "The index of value third is #{new_list.find('third')}"
 
-puts "The full list is #{list}"
+puts "The full list is #{new_list}"
+puts ' '
